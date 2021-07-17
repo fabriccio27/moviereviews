057ccbacc33dc26513369efdf4913a2d00a0765a
fabriccio27 <fabricio_inc27@hotmail.com>
2021-07-17 08:27:20 -0300

    tested permission_classes for ListAPIView

--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 #from django.views.generic import ListView
 
 from .models import Movie
 from .serializers import MovieSerializer
 # Create your views here.
 
-class MovieList(generics.ListCreateAPIView):
+class MovieList(generics.ListAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    permission_classes = (IsAuthenticated,)
