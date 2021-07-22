8c4f72960286f5ffdf3b6ccc597403cdb8a0c699
Fabricio Saavedra <fabricio_inc27@hotmail.com>
2021-07-21 21:33:28 -0300

    added staticfiles_storage in settings.py

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -136,6 +136,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "frontend/build/static")
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
